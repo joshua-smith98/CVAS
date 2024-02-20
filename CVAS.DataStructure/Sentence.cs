85df13624ac5df93e2eb4b5a88ef72f6f20903bb
Josh <jrichard.smith98@gmail.com>
2024-02-20 13:29:26 +1000

    Created Sentence class

---
+++ CVAS.DataStructure/Sentence.cs
@@ -0,0 +1,24 @@
+﻿using CVAS.AudioEngine;
+
+namespace CVAS.DataStructure
+{
+    public class Sentence : IPhrase
+    {
+        public string str { get; }
+        public string[] words { get; }
+
+        private SpokenPhrase[] _spokenPhrases;
+
+        internal Sentence(string str, string[] words, SpokenPhrase[] spokenPhrases)
+        {
+            this.str = str;
+            this.words = words;
+            this._spokenPhrases = spokenPhrases;
+        }
+
+        public IAudioClip GetAudioClip()
+        {
+            return new Playlist(_spokenPhrases.Select(x => x.GetAudioClip()).ToArray());
+        }
+    }
+}

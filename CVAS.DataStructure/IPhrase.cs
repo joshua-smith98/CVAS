2d33bd31f6686f71221bc5f70e7aff892481bc53
Josh <jrichard.smith98@gmail.com>
2024-02-20 12:57:32 +1000

    Created IPhrase interface

---
+++ CVAS.DataStructure/IPhrase.cs
@@ -0,0 +1,12 @@
+﻿using CVAS.AudioEngine;
+
+namespace CVAS.DataStructure
+{
+    public interface IPhrase
+    {
+        public string str { get; }
+        public string[] words { get; }
+
+        public IAudioClip GetAudioClip();
+    }
+}

b17c35d2347b1d750cc316501f3f93575562fac0
Josh <jrichard.smith98@gmail.com>
2024-02-27 11:16:56 +1000

    Created REPL project and IArgument interface

---
+++ CVAS.REPL/IArgument.cs
@@ -0,0 +1,10 @@
+﻿namespace CVAS.REPL
+{
+    public interface IArgument
+    {
+        public object? Value { get; }
+        public Type ValueType { get; }
+
+        public void ReadFrom();
+    }
+}

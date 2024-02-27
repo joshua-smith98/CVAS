042a2a50fbb2442027efa91e8864e0b48ecfd769
Josh <jrichard.smith98@gmail.com>
2024-02-27 14:14:21 +1000

    Created static Argument class

---
+++ CVAS.REPL/Argument.cs
@@ -0,0 +1,25 @@
+﻿namespace CVAS.REPL
+{
+    public static class Argument
+    {
+        private static readonly IArgument[] Arguments =
+        {
+
+        };
+
+        public static IArgument? ReadFrom(ref string str)
+        {
+            foreach (IArgument arg in Arguments)
+            {
+                try
+                {
+                    arg.ReadFrom(ref str);
+                    return arg; // Case: a valid argument is found
+                }
+                catch (ArgumentInvalidException) { }
+            }
+
+            return null; // Case: no valid argument is found
+        }
+    }
+}

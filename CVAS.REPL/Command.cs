85ab02175530a8cf886e60322727add213b63847
Josh <jrichard.smith98@gmail.com>
2024-02-27 14:19:16 +1000

    Created static Command class

---
+++ CVAS.REPL/Command.cs
@@ -0,0 +1,25 @@
+﻿namespace CVAS.REPL
+{
+    public static class Command
+    {
+        public static readonly ICommand[] Commands =
+        {
+
+        };
+
+        public static void RunFrom(ref string str)
+        {
+            foreach (ICommand command in Commands)
+            {
+                try
+                {
+                    command.RunFrom(ref str);
+                    return; // Case: a valid command is found
+                }
+                catch (CommandNotValidException) { }
+            }
+
+            throw new CommandNotValidException(); // Case: no valid commands found
+        }
+    }
+}

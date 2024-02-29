ec5fcc5e7b6b96764c5dda22888a7312ca41c835
Josh <jrichard.smith98@gmail.com>
2024-03-01 07:57:45 +1000

    Set REPL as default interface in anticipation of v0.5 release

--- CVAS.Main/Program.cs
+++ CVAS.Main/Program.cs
@@ -4,6 +4,9 @@ using CVAS.REPL;
 
 if (args.Length == 0)
 {
+    // Since the REPL is the only usable interface, we will set it as the default for now
+    REPL.Instance.Start();
+
     // Initialise XAML or WinForms ?
 }
 else if (args[0] == "-c")

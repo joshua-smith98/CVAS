3e8c637906dc8da21b7293307c597f63ed2cbfc2
Josh <jrichard.smith98@gmail.com>
2024-02-29 13:56:14 +1000

    Fixed some commands getting a NotImplementedException

--- CVAS.REPL/Commands/RenderCommand.cs
+++ CVAS.REPL/Commands/RenderCommand.cs
@@ -20,6 +20,19 @@
 
         public void RunFrom(string str)
         {
+            // Validity check: str must begin with "load"
+            if (!str.StartsWith(Str)) throw new CommandNotValidException();
+
+            // Try to read arguments
+            var temp_str = str.Substring(Str.Length + 1);
+
+            foreach (IArgument argument in Arguments) argument.ReadFrom(ref temp_str); // If this fails, an ArgumentNotValidException will be thrown, then caught by the REPL class.
+
+            // Validity check: str must be empty after all arguments are read
+            if (temp_str != "") throw new CommandNotValidException();
+
+            // Run Command
+
             throw new NotImplementedException(); // File rendering is yet to be implemented!
         }
     }

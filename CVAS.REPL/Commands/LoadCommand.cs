fea22edafd4a3aa1b84c8e88868a09cbcaeb8fa7
Josh <jrichard.smith98@gmail.com>
2024-02-29 12:10:30 +1000

    fixed variable name typo in LoadCommand

--- CVAS.REPL/Commands/LoadCommand.cs
+++ CVAS.REPL/Commands/LoadCommand.cs
@@ -28,7 +28,7 @@ namespace CVAS.REPL
             foreach (IArgument argument in Arguments) argument.ReadFrom(ref temp_str); // If this fails, an ArgumentNotValidException will be thrown, then caught by the REPL class.
 
             // Validity check: str must be empty after all arguments are read
-            if (str != "") throw new CommandNotValidException();
+            if (temp_str != "") throw new CommandNotValidException();
 
             var Path = Arguments[0].Value as string;
 

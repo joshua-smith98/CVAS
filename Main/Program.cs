f94988cf223b530295281246b22610c62d8c8709
Josh <jrichard.smith98@gmail.com>
2024-02-29 13:28:58 +1000

    Added main entry point and set up args check

---
+++ Main/Program.cs
@@ -0,0 +1,17 @@
+﻿// Main entry point
+
+using CVAS.REPL;
+
+if (args.Length == 0)
+{
+    // Initialise XAML or WinForms ?
+}
+else if (args[0] == "-c")
+{
+    // Initialise REPL
+    REPL.Instance.Start();
+}
+else // if args has some thing(s), but not -c
+{
+    // Initialise commandline
+}

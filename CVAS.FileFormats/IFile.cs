f97ec968fac1058deb732a0d8d28f49afb9a8846
Josh <jrichard.smith98@gmail.com>
2024-03-03 07:53:55 +1000

    Created IFile interface in anticipation of library caching

---
+++ CVAS.FileFormats/IFile.cs
@@ -0,0 +1,16 @@
+﻿namespace CVAS.FileFormats
+{
+    public interface IFile
+    {
+        public string? Path { get; }
+        public Type Type { get; }
+        public char[] Header { get; }
+
+        public static abstract void Validate();
+        public static abstract IFile LoadFrom(string path);
+        public static abstract IFile Deconstruct(object o);
+
+        public void SaveTo(string path);
+        public void Construct(ref object o);
+    }
+}

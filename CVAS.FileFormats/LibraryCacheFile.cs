0eaaf59cc9eb670a60a117c69f5ffcc73d3ebca9
Josh <jrichard.smith98@gmail.com>
2024-03-03 08:02:27 +1000

    Created LibraryCacheFile class

---
+++ CVAS.FileFormats/LibraryCacheFile.cs
@@ -0,0 +1,58 @@
+﻿using CVAS.DataStructure;
+
+namespace CVAS.FileFormats
+{
+    public class LibraryCacheFile : IFile
+    {
+        public string? Path { get; private set; }
+
+        public Type Type => typeof(Library);
+
+        public char[] Header => "CVASLBCH".ToArray();
+
+        private byte[] FolderHash;
+        private int NumPhrases;
+
+        private PhraseTableRow[] PhraseTable;
+
+        private LibraryCacheFile(string path, byte[] folderHash, int numPhrases, PhraseTableRow[] phraseTable)
+        {
+            Path = path;
+            FolderHash = folderHash;
+            NumPhrases = numPhrases;
+            PhraseTable = phraseTable;
+        }
+
+        private LibraryCacheFile(byte[] folderHash, int numPhrases, PhraseTableRow[] phraseTable)
+        {
+            FolderHash = folderHash;
+            NumPhrases = numPhrases;
+            PhraseTable = phraseTable;
+        }
+
+        public static IFile Deconstruct(object o)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static IFile LoadFrom(string path)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static void Validate()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Construct(ref object o)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SaveTo(string path)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

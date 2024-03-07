3107eec56fb749cc6fec92604e079fa8f527944d
Josh <jrichard.smith98@gmail.com>
2024-03-07 11:55:48 +1000

    Created interface IFolder

---
+++ CVAS.FileSystem/IFolder.cs
@@ -0,0 +1,12 @@
+﻿namespace CVAS.FileSystem
+{
+    public interface IFolder<T> where T : class
+    {
+        public string? Path { get; }
+
+        public IFolder<T> LoadFrom(string path);
+        public IFolder<T> Deconstruct(T Object);
+        public void SaveTo(string path);
+        public T Construct();
+    }
+}

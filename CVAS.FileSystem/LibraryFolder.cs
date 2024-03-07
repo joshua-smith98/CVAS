96077ca8bef8511809ba2460cda51082e2d19b5f
Josh <jrichard.smith98@gmail.com>
2024-03-07 11:57:39 +1000

    Created LibraryFolder class

---
+++ CVAS.FileSystem/LibraryFolder.cs
@@ -0,0 +1,29 @@
+﻿using CVAS.Core;
+
+namespace CVAS.FileSystem
+{
+    public class LibraryFolder : IFolder<Library>
+    {
+        public string? Path { get; private set; }
+
+        public Library Construct()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IFolder<Library> Deconstruct(Library Object)
+        {
+            throw new TransmutationNotAllowedException();
+        }
+
+        public IFolder<Library> LoadFrom(string path)
+        {
+            throw new TransmutationNotAllowedException();
+        }
+
+        public void SaveTo(string path)
+        {
+            throw new TransmutationNotAllowedException();
+        }
+    }
+}

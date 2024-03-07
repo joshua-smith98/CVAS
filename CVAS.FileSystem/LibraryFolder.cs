0a622f8f30339af71820b524510d3f9999a95433
Josh <jrichard.smith98@gmail.com>
2024-03-07 15:58:00 +1000

    Implemented LibraryFolder.LoadFrom()

--- CVAS.FileSystem/LibraryFolder.cs
+++ CVAS.FileSystem/LibraryFolder.cs
@@ -8,13 +8,60 @@ namespace CVAS.FileSystem
     {
         public string? Path { get; private set; }
 
-        public string[] FileNames { get; }
+        public string[] AudioFileNames { get; }
 
         public LibraryCacheFile? LibraryCacheFile { get; private set; }
 
-        private LibraryFolder(string[] FileNames)
+        private LibraryFolder(string[] audioFileNames)
         {
-            this.FileNames = FileNames;
+            AudioFileNames = audioFileNames;
+        }
+
+        public IFolder<Library> LoadFrom(string path)
+        {
+            // Validity checks: path is a valid directory
+            if (!Directory.Exists(path)) throw new DirectoryNotFoundException();
+
+            LibraryCacheFile libraryCacheFile;
+            List<string> audioFileNames  = new List<string>();
+
+            // Try to load LibraryCacheFile and extract AudioFileNames from that
+            try
+            {
+                libraryCacheFile = (LibraryCacheFile)LibraryCacheFile.LoadFrom(SysPath.Combine(path, "cache.lbc"));
+
+                foreach (LibraryCacheFile.PhraseTableRow phraseRow in libraryCacheFile.PhraseTable)
+                {
+                    foreach (LibraryCacheFile.InflectionTableRow inflectionRow in phraseRow.InflectionTable)
+                    {
+                        audioFileNames.Add(inflectionRow.AudioFileName);
+                    }
+                }
+
+                return new LibraryFolder(audioFileNames.ToArray()) { LibraryCacheFile = libraryCacheFile };
+            }
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine("A cache does not exist for this folder: a first-time analysis will be performed.");
+            }
+            catch (InvalidFileHashException)
+            {
+                Console.WriteLine("The folder contents have changed: a cache will be rebuilt.");
+            }
+
+            // Otherwise, load filenames from Directory.GetFiles(), checking for validity as audio files
+            foreach (string fileName in Directory.GetFiles(path))
+            {
+                try
+                {
+                    using var a = new AudioFileStreaming(fileName) ; // Test to see if it is a valid audio file
+
+                    audioFileNames.Add(SysPath.GetFileName(fileName));
+                }
+                catch { }
+            }
+
+            return new LibraryFolder(audioFileNames.ToArray());
         }
 
         public Library Construct()
@@ -25,11 +72,6 @@ namespace CVAS.FileSystem
         public IFolder<Library> Deconstruct(Library Object)
         {
             throw new TransmutationNotAllowedException();
-        }
-
-        public IFolder<Library> LoadFrom(string path)
-        {
-            throw new NotImplementedException();
         }
 
         public void SaveTo(string path)

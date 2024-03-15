7b3e91c6b45751bfaedeb917f14bb69ff95e9f03
Josh <jrichard.smith98@gmail.com>
2024-03-15 14:19:35 +1000

    Create and implemented new Command methods

--- CVAS.REPL/Command.cs
+++ CVAS.REPL/Command.cs
@@ -31,6 +31,30 @@
         /// Validates and attempts to perform this command's action, using the given string.
         /// </summary>
         /// <param name="str"></param>
-        public abstract void RunFrom(string str);
+        public void RunFrom(string str)
+        {
+            VerifyStr(str);
+            ReadArguments(str);
+            VerifyArgsAndRun();
+        }
+
+        private void VerifyStr(string str)
+        {
+            // Validity check: str must begin with "Str"
+            if (!str.StartsWith(Str)) throw new CommandNotValidException("Command does not match. This message should never be printed - if it was, open an issue on Github!");
+        }
+
+        private void ReadArguments(string str)
+        {
+            // Try to read arguments
+            var temp_str = str.Substring(Str.Length).TrimStart();
+
+            foreach (Argument argument in Arguments) temp_str = argument.ReadFrom(temp_str); // If this fails, an ArgumentNotValidException will be thrown, then caught by the REPL class.
+
+            // Validity check: str must be empty after all arguments are read
+            if (temp_str != "") throw new ArgumentNotValidException($"Expected end of command, found: '{temp_str}'!");
+        }
+
+        protected abstract void VerifyArgsAndRun();
     }
 }

0d64179da2dc26aa87c8f19eb8be6afb8462fb82
Josh <jrichard.smith98@gmail.com>
2024-03-21 12:39:17 +1000

    Created and implemented SentenceCmdLnArgument.cs

---
+++ CVAS.CommandLine/CmdLnArguments/SentenceCmdLnArgument.cs
@@ -0,0 +1,25 @@
+﻿namespace CVAS.CommandLine
+{
+    internal class SentenceCmdLnArgument : CmdLnArgument
+    {
+        public override string Str => "-sentence";
+
+        public override string? ShortStr => "-s";
+
+        public override string Description => "Provides a sentence to speak using the currently loaded library.";
+
+        protected override string[] ImportFromImpl(string[] args)
+        {
+            // Get sentence
+            var ret = args[1..];
+            var sentenceStr = ReadStringFromAndTrim(ref ret);
+
+            // Verify: Context.Library must exist!
+            if (CmdLnContext.Instance.Library is null) throw new CmdLnContextNotValidException("Cannot provide a sentence with no library loaded!");
+
+            // Save sentence and return trimmed args
+            CmdLnContext.Instance.Sentence = CmdLnContext.Instance.Library.GetSentence(sentenceStr);
+            return ret;
+        }
+    }
+}

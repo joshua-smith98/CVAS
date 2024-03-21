0616b89529aa884a74f66feed9bf338a3bef8089
Josh <jrichard.smith98@gmail.com>
2024-03-21 10:58:34 +1000

    Implemented CmdLnArgument logic

--- CVAS.CommandLine/CmdLnArgument.cs
+++ CVAS.CommandLine/CmdLnArgument.cs
@@ -8,13 +8,20 @@
 
         public string[] ImportFromAndTrim(string[] args)
         {
-            throw new NotImplementedException();
+            VerifyStr(args);
+            return ImportFromImpl(args);
         }
 
         private void VerifyStr(string[] args)
         {
-            // Check to see if Str or ShortStr matches
-            throw new NotImplementedException();
+            // Check that args[0] begins with a dash '-'
+            if (args[0][0] != '-') throw new CmdLnStrNotValidException("Given option does not begin with '-'.");
+
+            // Trim dash from args[0]
+            var trimmedArg = args[0][1..].ToLower();
+
+            // Check trimmed args[0] to Str and ShortStr
+            if (trimmedArg != Str && trimmedArg != ShortStr) throw new CmdLnStrNotValidException("Given option does not match Str or ShortStr.")
         }
 
         protected abstract string[] ImportFromImpl(string[] args);

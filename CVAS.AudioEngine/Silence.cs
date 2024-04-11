c6bbe8456fe2e4523d3c2db62afaec448def710d
Josh <jrichard.smith98@gmail.com>
2024-04-12 09:39:05 +1000

    Created and implemented new Silence class

---
+++ CVAS.AudioEngine/Silence.cs
@@ -0,0 +1,54 @@
+﻿using System.IO;
+using Un4seen.Bass;
+
+namespace CVAS.AudioEngineNS
+{
+    public class Silence(int milliseconds) : AudioClip
+    {
+        public int Milliseconds => milliseconds;
+
+        internal override int GetStreamHandle()
+        {
+            // Get length in bytes
+            var byteLength = (int)((milliseconds / 1000f) * AudioEngine.Instance.Freq * 2); // 2 is the number of bytes per sample (16bit audio)
+
+            // Create sample
+            int sampleHandle = Bass.BASS_SampleCreate(byteLength, AudioEngine.Instance.Freq, 1, 1, BASSFlag.BASS_DEFAULT);
+            if (sampleHandle == 0)
+            {
+                // Handle errors
+                var bassError = Bass.BASS_ErrorGetCode();
+                switch (bassError)
+                {
+                    case BASSError.BASS_ERROR_INIT:
+                        throw new AudioEngineException("Attempted to open audio file before BASS was initialised!");
+                    case BASSError.BASS_ERROR_MEM:
+                        throw new AudioEngineException("BASS ran out of memory while generating silence.");
+                    default:
+                        throw new AudioEngineException($"An unknown error occurred while generating silence. BASS error code: {bassError}");
+                }
+            }
+
+            // Generate sample bytes
+            if (!Bass.BASS_SampleSetData(sampleHandle, new short[byteLength]))
+            {
+                // Handle unknown error
+                var bassError = Bass.BASS_ErrorGetCode();
+                throw new AudioEngineException($"An unknown error occurred while generating silence. BASS error code: {bassError}");
+            }
+
+            // Get stream handle
+            int streamHandle = Bass.BASS_SampleGetChannel(sampleHandle, BASSFlag.BASS_SAMCHAN_STREAM | BASSFlag.BASS_STREAM_DECODE);
+            if (streamHandle == 0)
+            {
+                // No errors should be thrown here if the code is semantically correct, so we'll only use the default "unknown error" message.
+                var bassError = Bass.BASS_ErrorGetCode();
+                throw new AudioEngineException($"An unknown error occurred while generating silence. BASS error code: {bassError}");
+            }
+
+            // Run autofree and return handle
+            Task.Run(() => AudioEngine.FreeUponStop(streamHandle));
+            return streamHandle;
+        }
+    }
+}

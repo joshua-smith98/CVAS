05b258b2c3ce9b6858d00abe969f31fefa5b08a1
Josh <jrichard.smith98@gmail.com>
2024-04-12 11:11:36 +1000

    Created and implemented new Playlist class in CVAS.AudioEngine

---
+++ CVAS.AudioEngine/Playlist.cs
@@ -0,0 +1,45 @@
+﻿using Un4seen.Bass;
+using Un4seen.Bass.AddOn.Mix;
+
+namespace CVAS.AudioEngineNS
+{
+    public class Playlist(params AudioClip[] audioClips) : AudioClip
+    {
+        public AudioClip[] AudioClips => audioClips;
+
+        internal override int GetStreamHandle()
+        {
+            // Create mixer
+            int mixerHandle = BassMix.BASS_Mixer_StreamCreate(
+                AudioEngine.Instance.Freq,
+                AudioEngine.Instance.Chans,
+                BASSFlag.BASS_MIXER_END |
+                BASSFlag.BASS_MIXER_QUEUE |
+                BASSFlag.BASS_STREAM_DECODE
+                );
+             
+            if (mixerHandle == 0)
+            {
+                // Handle errors
+                var bassError = Bass.BASS_ErrorGetCode();
+                switch (bassError)
+                {
+                    case BASSError.BASS_ERROR_INIT:
+                        throw new AudioEngineException("Attempted to create a playlist before BASS was initialised!");
+                    case BASSError.BASS_ERROR_MEM:
+                        throw new AudioEngineException("BASS ran out of memory while creating playlist.");
+                    default:
+                        throw new AudioEngineException($"An unknown error occurred while creating a playlist. BASS error code: {bassError}");
+                }
+            }
+
+            // Add audioClips to mixer
+            foreach (AudioClip audioClip in AudioClips)
+                BassMix.BASS_Mixer_StreamAddChannel(mixerHandle, audioClip.GetStreamHandle(), BASSFlag.BASS_MIXER_CHAN_DOWNMIX);
+
+            // Run autofree and return handle
+            Task.Run(() => AudioEngine.FreeUponStop(mixerHandle));
+            return mixerHandle;
+        }
+    }
+}

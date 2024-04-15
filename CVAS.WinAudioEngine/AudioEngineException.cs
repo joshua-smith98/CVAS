eae15fdb4095bcc75c1df181b39a4d1f33bc67df
Josh <jrichard.smith98@gmail.com>
2024-04-15 16:20:31 +1000

    Fixed incorrect namespace in WinAudioEngine AudioEngineException

--- CVAS.WinAudioEngine/AudioEngineException.cs
+++ CVAS.WinAudioEngine/AudioEngineException.cs
@@ -1,4 +1,4 @@
-﻿namespace CVAS.WinAudioEngine
+﻿namespace CVAS.WinAudioEngineNS
 {
     public class AudioEngineException(string message) : Exception(message);
     // Unused in CVAS.WinAudioEngine, but required for compatibility with CVAS.AudioEngine

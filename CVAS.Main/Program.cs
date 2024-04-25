56728a6ee6de7f82017d4e0d9a4f57c6108d370e
joshua-smith98 <jrichard.smith98@gmail.com>
2024-04-26 09:51:19 +1000

    Implemented using for IDisposeable singletons (Terminal and AudioEngine) in entry point

--- CVAS.Main/Program.cs
+++ CVAS.Main/Program.cs
@@ -5,33 +5,39 @@ using CVAS.TerminalNS;
 
 // Main entry point
 
+using (Terminal.Instance) // Use terminal for duration of try/catch scope so that we can still print the fatal error message
+
 #if !DEBUG // Only catch unhandled exceptions on release
-try
-{
-#endif
-    if (args.Length == 0) REPL.Instance.Start(); // Start REPL when run with no args
-    else
+    try
     {
-        // Silence Terminal, initialise command-line and read from args
-        Terminal.Instance.IsSilent = true;
-        CmdLnContext.Instance.ReadFromAndRun(args);
-    }
+#endif
+        if (args.Length == 0)
+        {
+            using (IAudioEngine.Instance) // Use AudioEngine instance in the scope of the REPL
+                REPL.Instance.Start(); // Start REPL when run with no args
+        }
+        else
+        {
+            // Silence Terminal, initialise command-line and read from args
+            Terminal.Instance.IsSilent = true;
+            CmdLnContext.Instance.ReadFromAndRun(args);
+        }
 #if !DEBUG
-}
-catch(Exception e)
-{
-    // Print unknown error message for misc exceptions
-    Terminal.Instance.IsSilent = false; // just in case
+    }
+    catch(Exception e)
+    {
+        // Print unknown error message for misc exceptions
+        Terminal.Instance.IsSilent = false; // just in case
 
-    Terminal.Instance.ForceBeginMessage();
-    Terminal.Instance.Message($"A fatal error has occurred, of type: {e.GetType()}", ConsoleColor.Red);
-    Terminal.Instance.Message();
-    Terminal.Instance.Message($"\"{e.Message}\"", ConsoleColor.Red);
-    Terminal.Instance.Message($"{e.StackTrace}", ConsoleColor.Red);
-    Terminal.Instance.Message();
-    Terminal.Instance.Message("Please take a screenshot of this, and create an issue on github.", ConsoleColor.Red);
-    Terminal.Instance.EndMessage();
+        Terminal.Instance.ForceBeginMessage();
+        Terminal.Instance.Message($"A fatal error has occurred, of type: {e.GetType()}", ConsoleColor.Red);
+        Terminal.Instance.Message();
+        Terminal.Instance.Message($"\"{e.Message}\"", ConsoleColor.Red);
+        Terminal.Instance.Message($"{e.StackTrace}", ConsoleColor.Red);
+        Terminal.Instance.Message();
+        Terminal.Instance.Message("Please take a screenshot of this, and create an issue on github.", ConsoleColor.Red);
+        Terminal.Instance.EndMessage();
 
-    Terminal.Instance.AwaitKey("Press any key to close the application...");
-}
+        Terminal.Instance.AwaitKey("Press any key to close the application...");
+    }
 #endif
